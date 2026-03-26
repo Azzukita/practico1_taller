d3b442a9d13bd831780994ae97261656236f3f25
Lucas <lucas.martina.181685@alumnos.econo.unlp.edu.ar>
2026-03-25 23:56:13 -0300

    tp1, punto 7, tercer subinciso y correcciones

--- questions.py
+++ questions.py
@@ -1,23 +1,37 @@
 import random
-words = [
-    "python",
-    "programa",
-    "variable",
-    "funcion",
-    "bucle",
-    "cadena",
-    "entero",
-    "lista",
-]
-
-word = random.choice(words)
-guessed = []
-attempts = 6
-puntos = 0 
+categorias = {
+    "Programacion" : ["python", "programa", "variable", "funcion", "bucle", "cadena", "entero", "lista"],
+    "Cuadros" : ["nacional", "gimnasia", "independiente", "sarmiento", "boca", "river", "brown"],
+    "Flores" : ["rosa", "tulipan", "margarita", "orquidea", "jazmin", "lavanda", "girasol", "clavel"]
+}
 
 print("¡Bienvenido al Ahorcado!")
+print("Categorías disponibles:")
+nombres_categorias = list(categorias.keys())
+for i, cat in enumerate(nombres_categorias, start=1):
+    print(f"  {i}. {cat}")  # ← muestra el número junto al nombre
 print()
 
+eleccion = input("Elegí una categoría (número): ")
+categorias = list(categorias.keys())
+
+if eleccion == "1":
+    categoria_elegida = categorias[0]
+elif eleccion == "2":
+    categoria_elegida = categorias[1]
+elif eleccion == "3":
+    categoria_elegida = categorias[2]
+else:
+    print("Opción no válida, se elige programación por defecto.")
+    categoria_elegida = categorias[0]
+
+word = random.choice(categorias[categoria_elegida])
+print(f"Categoría elegida: {categoria_elegida}")
+print()
+
+guessed = []
+attempts = 6
+puntos = 0
 while attempts > 0:
     # Mostrar progreso: letras adivinadas y guiones para las que faltan
     progress = ""
@@ -29,6 +43,7 @@ while attempts > 0:
     print(progress)
     # Verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
+        puntos += 6
         print("¡Ganaste!")
         print (f"puntaje final: {puntos}")
         break
@@ -50,7 +65,7 @@ while attempts > 0:
                 print()
         else:
             print ("entrada no válida")        
-    else:
-        puntos = 0
-        print(f"¡Perdiste! La palabra era: {word}")
-        print(f"Puntaje final: {puntos}")
+else:
+    puntos = 0
+    print(f"¡Perdiste! La palabra era: {word}")
+    print(f"Puntaje final: {puntos}")
