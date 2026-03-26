37c898397db7b0e174d5772a7b7c480bfd3fdfc9
Lucas <lucas.martina.181685@alumnos.econo.unlp.edu.ar>
2026-03-25 23:35:00 -0300

    tp1, punto 7, modificacion para  segundo subinciso y correcciones

--- questions.py
+++ questions.py
@@ -13,6 +13,7 @@ words = [
 word = random.choice(words)
 guessed = []
 attempts = 6
+puntos = 0 
 
 print("¡Bienvenido al Ahorcado!")
 print()
@@ -24,17 +25,18 @@ while attempts > 0:
         if letter in guessed:
             progress += letter + " "
         else:
-            progress += " "
+            progress += "_ "
     print(progress)
     # Verificar si el jugador ya adivinó la palabra completa
-    if "" not in progress:
+    if "_" not in progress:
         print("¡Ganaste!")
+        print (f"puntaje final: {puntos}")
         break
     elif "_" in progress:
         print(f"Intentos restantes: {attempts}")
         print(f"Letras usadas: {', '.join(guessed)}")
         letter = input("Ingresá una letra: ")
-        if len(letter) == 1 and "a" <= letter <= "Z": 
+        if len(letter) == 1 and letter.isalpha(): 
             if letter in guessed:
                 print("Ya usaste esa letra.")
             elif letter in word:
@@ -43,9 +45,12 @@ while attempts > 0:
             else:
                 guessed.append(letter)
                 attempts -= 1
+                puntos -= 1
                 print("Esa letra no está en la palabra.")
                 print()
         else:
             print ("entrada no válida")        
     else:
+        puntos = 0
         print(f"¡Perdiste! La palabra era: {word}")
+        print(f"Puntaje final: {puntos}")
